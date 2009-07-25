00f0496906e4c13f31228bf490bed8fc68b53b1d
Anton <Anton@bender>
2009-07-25 13:29:16 +0200

    Added support.py

---
+++ Support.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+import os, sys
+import pygame
+from pygame.locals import *
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('images')
+    fullname = os.path.join(fullname, name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error, message:
+        print 'Cannot load image:', fullname
+        raise SystemExit, message
+    image = image.convert()
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0,0))
+        image.set_colorkey(colorkey, RLEACCEL)
+    return image
